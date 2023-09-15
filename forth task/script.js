72a1c0b44362591fd3547acc109af3f282c24ba7
Mohamed Amr Farouk <mohamedamr_2014@yahoo.com>
2023-09-15 19:36:56 +0300

    forth task issue sovled

--- forth task/script.js
+++ forth task/script.js
@@ -1,61 +1,54 @@
-// Method to upload a valid csv file
+var jsonData = [];
+var headers = [];
+
 function upload() {
-    console.log("5")
-    var files = document.getElementById('file_upload').files;
-    console.log("2")
-    if(files.length==0){
-        alert("Please choose any file...");
-        console.log("3")
-    }
-    var filename = files[0].name;
-    var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
-        //Here calling another method to read CSV file into json
-        return files[0];
+  var files = document.getElementById('file_upload').files;
+  if (files.length == 0) {
+    alert("Please choose any file...");
+  }
+  var filename = files[0].name;
+  var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+  return files[0];
 }
 
-//Method to read csv file and convert it into JSON 
-function csvFileToJSON(file){
-    // debugger;
-    console.log(file)
+async function csvFileToJSON(file) {
+  return new Promise((resolve, reject) => {
     try {
-        var reader = new FileReader();
-        reader.readAsBinaryString(file);
-        reader.onload = function(e) {
-            var jsonData = [];
-            var headers = [];
-            var rows = e.target.result.split("\r\n");               
-            for (var i = 0; i < rows.length; i++) {
-                var cells = rows[i].split(",");
-                var rowData = {};
-                for(var j=0;j<cells.length;j++){
-                    if(i==0){
-                        var headerName = cells[j].trim();
-                        headers.push(headerName);
-                    }else{
-                        var key = headers[j];
-                        if(key){
-                            rowData[key] = cells[j].trim();
-                        }
-                    }
-                }
-                
-            if(i!=0){
-                jsonData.push(rowData);
+      var reader = new FileReader();
+      reader.readAsBinaryString(file);
+      reader.onload = function (e) {
+        var rows = e.target.result.split("\n");
+        for (var i = 0; i < rows.length; i++) {
+          var cells = rows[i].split(",");
+          var rowData = {};
+          for (var j = 0; j < cells.length; j++) {
+            if (i === 0) {
+              var headerName = cells[j].trim();
+              headers.push(headerName);
+            } else {
+              var key = headers[j];
+              if (key) {
+                rowData[key] = cells[j].trim();
+              }
             }
+          }
+          if (i !== 0) {
+            jsonData.push(rowData);
+          }
         }
-
-        //displaying the json result into HTML table
-        console.log(jsonData)
-        return jsonData;
-        }
-    }catch(e){
-        console.error(e);
+        resolve(jsonData);
+      };
+    } catch (e) {
+      console.error(e);
+      reject(e);
     }
+  });
 }
 
-//Method to display the data in HTML Table
+
 function displayJsonToHtmlTable(jsonData){
     var table=document.getElementById("display_csv_data");
+    table.innerHTML=''
     if(jsonData.length>0){
         var headers = Object.keys(jsonData[0]);
         var htmlHeader='<thead><tr>';
@@ -84,28 +77,20 @@ function displayJsonToHtmlTable(jsonData){
 
 
 
-const btn = document.querySelector("#btn")
+
+const btn = document.querySelector("#btn");
 
 
-function btn_func(){
-    myfile = upload()
-    jsonData = csvFileToJSON(myfile)
-    displayJsonToHtmlTable(jsonData)
+async function btn_func() {
+    jsonData=[]
+    try {
+      const myfile = upload();
+      const jsonData = await csvFileToJSON(myfile);
 
-}
-// async function btn_func(){
-//     const mypromise = new Promise(async (resolve, reject)=>{
-//         myfile = upload()
-//         jsonData = csvFileToJSON(myfile)
-    
-//         if(jsonData){
-//             resolve(jsonData)
-//         }else{
-//             reject(Error("file not valid"))
-//         }
-//     });
-//         console.log("before")
-//         let value = await mypromise
-//         displayJsonToHtmlTable(value)
-//         console.log('after')
-// }
+      displayJsonToHtmlTable(jsonData)
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
